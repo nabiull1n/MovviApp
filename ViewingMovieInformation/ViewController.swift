f58c1616da74e3307815210f218d3d3b9af45cd1
Denis <nabiullin.den3@gmail.com>
2023-02-11 12:55:13 +0500

    Initial Commit

---
+++ ViewingMovieInformation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewingMovieInformation
+//
+//  Created by Денис Набиуллин on 11.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
